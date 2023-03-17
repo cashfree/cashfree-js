e8e998b53deb7f409d2ba881ce7079800fe35d40
Raj Nandan Sharma <raj@Raj-Nandan-Sharma.local>
2023-03-17 14:52:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Cashfree.js ES Module
+
+Use [Cashfree.js](https://docs.cashfree.com/docs/js-integration) as an ES module.
+
+## Installation
+
+Use `npm` to install the Cashfree.js module:
+
+```sh
+npm install @cashfree/cashfree-js
+```
+
+## Usage
+
+### `load`
+
+This function returns a `Promise` that resolves with a newly created `Cashfree` object once Cashfree.js has loaded. 
+If you call `load` in a server environment it will resolve to `null`.
+
+```js
+import {load} from '@cashfree/cashfree-js';
+
+const cashfree = await load();
+```
+
+
+For more information on how to use Cashfree.js, please refer to the [Cashfree.js API reference](https://docs.cashfree.com/docs/getting-started) or learn to [accept a payment](https://docs.cashfree.com/docs/getting-started#pay) with Cashfree.
+
+If you have deployed a [Content Security Policy](https://developer.mozilla.org/en-US/docs/Web/Security/CSP), make sure to [include Cashfree.js in your directives].
+-   `connect-src`, `https://api.cashfree.com` 
+-   `frame-src`, `https://sdk.cashfree.com`, `https://api.cashfree.com`, `https://sandbox.cashfree.com` `https://payments.cashfree.com`, `https://payments-test.cashfree.com`
+-   `script-src`, `https://sdk.cashfree.com`
+
+## Cashfree.js Documentation
+
+- [Cashfree.js Docs](https://docs.cashfree.com/docs/getting-started)
+- [API Reference](https://docs.cashfree.com/reference)
---
+++ dist/script.js
@@ -0,0 +1,113 @@
+var V3_URL = "https://sdk.cashfree.com/js/v3/cashfree.js";
+var V3_URL_REGEX = /^https:\/\/sdk\.cashfree\.com\/js\/v3\/?(\?.*)?$/;
+var EXISTING_SCRIPT_MESSAGE = "load was called but an existing Cashfree.js script already exists in the document; existing script parameters will be used";
+var findScript = function findScript() {
+    var scripts = document.querySelectorAll('script[src^="'.concat(V3_URL, '"]'));
+
+    for (var i = 0; i < scripts.length; i++) {
+        var script = scripts[i];
+
+        if (!V3_URL_REGEX.test(script.src)) {
+            continue;
+        }
+
+        return script;
+    }
+
+    return null;
+};
+
+var injectScript = function injectScript(params) {
+    var queryString = params && !params.advancedFraudSignals ? "?advancedFraudSignals=false" : "";
+    var script = document.createElement("script");
+    script.src = "".concat(V3_URL).concat(queryString);
+    var headOrBody = document.head || document.body;
+
+    if (!headOrBody) {
+        throw new Error("Expected document.body not to be null. Cashfree.js requires a <body> element.");
+    }
+
+    headOrBody.appendChild(script);
+    return script;
+};
+
+var cashfreePromise = null;
+var loadScript = function loadScript(params) {
+    // Ensure that we only attempt to load Cashfree.js at most once
+    if (cashfreePromise !== null) {
+        return cashfreePromise;
+    }
+
+    cashfreePromise = new Promise(function (resolve, reject) {
+        if (typeof window === "undefined" || typeof document === "undefined") {
+            // Resolve to null when imported server side. This makes the module
+            // safe to import in an isomorphic code base.
+            resolve(null);
+            return;
+        }
+
+        if (window.Cashfree && params) {
+            console.warn(EXISTING_SCRIPT_MESSAGE);
+        }
+
+        if (window.Cashfree) {
+            resolve(window.Cashfree);
+            return;
+        }
+
+        try {
+            var script = findScript();
+
+            if (script && params) {
+                console.warn(EXISTING_SCRIPT_MESSAGE);
+            } else if (!script) {
+                script = injectScript(params);
+            }
+
+            script.addEventListener("load", function () {
+                if (window.Cashfree) {
+                    resolve(window.Cashfree);
+                } else {
+                    reject(new Error("Cashfree.js not available"));
+                }
+            });
+            script.addEventListener("error", function () {
+                reject(new Error("Failed to load Cashfree.js"));
+            });
+        } catch (error) {
+            reject(error);
+            return;
+        }
+    });
+    return cashfreePromise;
+};
+var initCashfree = function initCashfree(maybeCashfree, args, startTime) {
+    if (maybeCashfree === null) {
+        return null;
+    }
+
+    var cashfree = maybeCashfree.apply(undefined, args);
+    return cashfree;
+};
+
+var cashfreePromise$1 = Promise.resolve().then(function () {
+    return loadScript(null);
+});
+var loadCalled = false;
+cashfreePromise$1["catch"](function (err) {
+    if (!loadCalled) {
+        console.warn(err);
+    }
+});
+var load = async function load() {
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+    }
+
+    loadCalled = true;
+    var startTime = Date.now();
+    const maybeCashfree = await cashfreePromise$1;
+    return initCashfree(maybeCashfree, args, startTime);
+};
+
+export { load };
